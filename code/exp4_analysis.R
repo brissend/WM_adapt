948d3ecab396a14d4c6a01e0a17c87cd3113e293
brissend <brissend725@gmail.com>
2024-09-05 10:42:06 -0400

    added n-1 Att-error performance analysis (Exp 4)

--- code/exp4_analysis.R
+++ code/exp4_analysis.R
@@ -30,6 +30,10 @@ for (s in seq_along(subs)) {
   sub_df_fixed$n1RT = c(rep(NA,25),
                         subdf[which((subdf$phase == 'adapt') & (subdf$trialType == 'memory'))-1,]$RT,
                         rep(NA,25))
+  sub_df_fixed$n1acc = c(rep(NA,25),
+                         subdf[which((subdf$phase == 'adapt') & (subdf$trialType == 'memory'))-1,]$adaptCorrect,
+                         rep(NA,25))
+  sub_df_fixed$memDiff = c(0,diff(sub_df_fixed$memResponse))
   grpdf[[s]] = sub_df_fixed
   
   # WM-random
@@ -123,6 +127,34 @@ bf_full = lmBF(memResponse ~ trial + subject,
 bf_int = lmBF(memResponse ~ subject,
               whichRandom = "subject",
               data = filter(grpdf,phase == 'pre-adapt') %>% drop_na(memResponse))
+
+bf_full/bf_int
+
+# Bayes factor analysis examining effect n-1 Att-error trial performance on adaptation
+grpdf$subject = factor(grpdf$subject)
+set.seed(1111)
+bf_full = lmBF(memDiff ~ n1RT + subject, 
+               whichRandom = 'subject',
+               rscaleRandom = 'nuisance',
+               data = grpdf %>% drop_na(memDiff,n1RT))
+
+bf_int = lmBF(memDiff ~ subject, 
+              whichRandom = "subject",
+              rscaleRandom = 'nuisance',
+              data = grpdf %>% drop_na(memDiff,n1RT))
+
+bf_full/bf_int
+
+set.seed(1111)
+bf_full = lmBF(memDiff ~ n1acc + subject, 
+               whichRandom = 'subject',
+               rscaleRandom = 'nuisance',
+               data = grpdf %>% drop_na(memDiff,n1acc))
+
+bf_int = lmBF(memDiff ~ subject, 
+              whichRandom = "subject",
+              rscaleRandom = 'nuisance',
+              data = grpdf %>% drop_na(memDiff,n1acc))
 
 bf_full/bf_int
 
